74801fc63817820fccc73350ddb83930ab840532
zyx <whyzi@qq.com>
2018-08-03 17:32:58 +0800

    zyx

---
+++ test.py
@@ -0,0 +1,6 @@
+# import tensorflow as tf
+# classifer=tf.estimator.LinearClassfier()
+# classifer.train(input_fu=train_input_fn,steps=2000)
+# predictions=classifer.predict(input_fu=predict_input_fn)
+import pandas as pd
+pd.__version__
