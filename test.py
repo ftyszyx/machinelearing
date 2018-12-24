eb472921f153c91e61efd5b54dd02fb775552e6b
zyx <whyzi@qq.com>
2018-12-24 17:11:44 +0800

    zyx

--- test.py
+++ test.py
@@ -2,5 +2,8 @@
 # classifer=tf.estimator.LinearClassfier()
 # classifer.train(input_fu=train_input_fn,steps=2000)
 # predictions=classifer.predict(input_fu=predict_input_fn)
-import pandas as pd
-pd.__version__
+import numpy as np
+import pandas as pd 
+dateset=pd.read_csv("Data.csv")
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc
